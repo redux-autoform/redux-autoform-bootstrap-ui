4a57c0984b406e9756bb7fcebe0608d130a9d783
daniel.gomez <daniel.gomez@samsistemas.com.ar>
2016-07-21 11:59:06 -0300

    Added Tests

--- test/createFactory.js
+++ test/createFactory.js
@@ -1,12 +1,44 @@
 import { BootstrapFactory } from '../src/index';
+import { expect } from 'chai';
 
 
 describe("Factory creation", function() {
     it("User requests an edit factory", function(done) {
+
+        let correctConfig = {
+            'string': 'TextBox',
+            'array': 'ArrayContainer',
+            'datetime': 'DateTimePicker',
+            'time': 'DateTimePicker',
+            'date': 'DateTimePicker',
+            'int' : 'TextBox',
+            'float': 'TextBox',
+            'bool': 'Checkbox',
+            'group': 'FieldGroup'
+        };
+
+        let factory = BootstrapFactory.createComponentFactory("edit");
+
+        expect(factory.defaultFieldComponents).to.be.deep.equal(correctConfig);
         done()
     })
 
     it("User requests an details factory", function(done) {
+        let correctConfig = {
+            'string': 'Static',
+            'array': 'ArrayContainer',
+            'datetime': 'Static',
+            'time': 'Static',
+            'date': 'Static',
+            'int': 'Static',
+            'float': 'Static',
+            'bool': 'Static',
+            'group': 'FieldGroup'
+        };
+
+        let factory = BootstrapFactory.createComponentFactory("details");
+
+        expect(factory.defaultFieldComponents).to.be.deep.equal(correctConfig);
         done()
     })
 });
