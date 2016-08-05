d44573eb7347dd12ec8bc7bf83de19bc64768780
jonisaa <jonatansalas@live.com.ar>
2016-08-05 11:17:00 -0300

    bump to 1.1.3

--- src/components/fieldComponents/ArrayContainer.js
+++ src/components/fieldComponents/ArrayContainer.js
@@ -92,7 +92,7 @@ class ArrayContainer extends Component {
     };
     
     getAllComponents = () => {
-        //TODO: We should replace href calls with Link from react-router
+        //TODO: We should replace a for button!
         let components = this.getComponents();
         
         if (components.length) {
@@ -100,7 +100,7 @@ class ArrayContainer extends Component {
         } else {
             return (
                 <Alert bsStyle="warning">
-                    This array is empty. Consider <a href="" onClick={ this.handleAdd }>adding a new item</a>.
+                    This array is empty. Consider <a onClick={ this.handleAdd }>adding a new item</a>.
                 </Alert>
             );
         }
