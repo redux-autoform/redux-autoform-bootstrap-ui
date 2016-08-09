e843390126f8055326fed6c6c9ac0efac5e4762f
André Pena <andrerpena@gmail.com>
2016-08-09 20:22:46 -0300

    #24 Just making it a little bit better

--- src/components/group/Group.js
+++ src/components/group/Group.js
@@ -1,84 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import AlertMessage from '../common/AlertMessage';
 import NormalForm from './../form/NormalForm';
+import BaseGroup from './BaseGroup';
 
-class Group extends Component {
+class Group extends BaseGroup {
     static propTypes = {
         component: PropTypes.string,
         fields: PropTypes.array.isRequired,
         layout: PropTypes.object.isRequired,
         componentFactory: PropTypes.object.isRequired
-    };
-    
-    getComponents = () => {
-        let { layout, componentFactory, fields } = this.props;
-        let components;
-        
-        if (layout.fields) {
-            
-            components = layout.fields.map(field => {
-                let fieldMetadata = fields.find(cp => cp.name === field.name);
-                
-                if (!fieldMetadata) {
-                    throw Error(`Could not find field. Field: ${field.name}`);
-                }
-
-                // in case the field is going to render layouts internally, it's going to need information about the
-                // layout and fields. I'm not sure if this is the best way to do it, probably not. TODO: Review it.
-                fieldMetadata._extra = {layout, fields};
-
-                return {
-                    data: fieldMetadata,
-                    length: layout.fields.length,
-                    component: componentFactory.buildFieldComponent(fieldMetadata)
-                }
-            });
-            
-        } else if (layout.groups) {
-            
-            components = layout.groups.map(group => {
-                return {
-                    data: group,
-                    length: layout.groups.length,
-                    component: componentFactory.buildGroupComponent({
-                        component: group.component,
-                        layout: group,
-                        fields: fields,
-                        componentFactory: componentFactory
-                    })
-                }
-            });
-        }
-        
-        return components;
-    };
-    
-    getDefaultSize = (component, gridLength = 12) => {
-        let { layout } = this.props;
-        
-        return layout.orientation == 'horizontal' ? Math.floor(gridLength/component.length) : gridLength;
-    };
-    
-    getContent = () => {
-        let components = this.getComponents();
-        
-        return components.map((component, i) => {
-            let componentContent, size = 12;
-
-            // invisible components should be hidden
-            if (component.data.visible === false) {
-                componentContent = null;
-            } else {
-                size = component.data.size || this.getDefaultSize(component);
-                componentContent = component.component;
-            }
-
-            return (
-                <div key={`component-${i}-wrapper`} className={`col-md-${size}`}>
-                    { componentContent }
-                </div>
-            );
-        });
     };
 
     render() {
