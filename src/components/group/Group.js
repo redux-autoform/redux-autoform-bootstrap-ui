4a411a24c21710c3a0ad0dd98bcec193f3d96a6e
André Pena <andrerpena@gmail.com>
2016-08-09 20:27:21 -0300

    #24 Removing the NormalForm

--- src/components/group/Group.js
+++ src/components/group/Group.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import AlertMessage from '../common/AlertMessage';
-import NormalForm from './../form/NormalForm';
+import Header from '../common/Header';
 import BaseGroup from './BaseGroup';
 
 class Group extends BaseGroup {
@@ -12,16 +11,23 @@ class Group extends BaseGroup {
     };
 
     render() {
-        let { layout } = this.props;
 
-        // the passed in layout can contain either fields or groups.
-        // in case it contains 'fields', we're gonna render each of the fields.
-        // in case it contains 'groups', we're gonna render each group, passing the fields as a parameter
-        try {
-            return <NormalForm title={layout.title} content={this.getContent()}/>
-        } catch (ex) {
-            return <AlertMessage error={ex}/>
-        }
+        let { layout } = this.props;
+        let content = this.getContent();
+
+        return (
+            <section>
+                <div className='row'>
+                    <div className="metaform-group">
+                        <Header title={layout.title}/>
+                        <div className="metaform-group-content">
+                            { content }
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+
     }
 }
 
