88a6f7d8708af2795b6dd8d3f6a193a2b32f0411
jonisaa <jonatansalas@live.com.ar>
2016-08-10 18:02:32 -0300

    #2 Refactor to es6 class

--- src/components/common/FileInfo.js
+++ src/components/common/FileInfo.js
@@ -2,31 +2,34 @@ import React, { Component, PropTypes } from 'react';
 import { Col } from 'react-bootstrap';
 import filesize from 'filesize';
 
-const FileUploadItem = ({ file, height, width }) => {
-	let image = null;
+export default class FileUploadItem extends Component {
+	static propTypes = {
+		onClick: PropTypes.func,
+		file: PropTypes.object.isRequired,
+		height: PropTypes.string,
+		width: PropTypes.string
+	};
 
-	if (file.type.match(/image/)) {
-		image = <img height={height} width={width} src={file.preview}/>;
+	static defaultProps = {
+		height: "150px",
+		width: "150px"
+	};
+
+	render() {
+		let { file, height, width } = this.props;
+
+		let image = null;
+
+		if (file.type.match(/image/)) {
+			image = <img height={height} width={width} src={file.preview}/>;
+		}
+
+		return (
+			<Col xs={4} md={4}>
+				{image}
+				<p>{file.name}</p>
+				<p>{filesize(file.size)}</p>
+			</Col>
+		);
 	}
-
-	return (
-		<Col xs={4} md={4}>
-			{image}
-			<p>{file.name}</p>
-			<p>{filesize(file.size)}</p>
-		</Col>
-	);
 };
-
-FileUploadItem.propTypes = {
-	file: PropTypes.object.isRequired,
-	height: PropTypes.string,
-	width: PropTypes.string
-};
-
-FileUploadItem.defaultProps = {
-	height: "150px",
-	width: "150px"
-};
-
-export default FileUploadItem;
