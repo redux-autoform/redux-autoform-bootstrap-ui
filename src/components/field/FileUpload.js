c1ab9c9b9ce80ddac3d882ddc2105d27561750b5
jonisaa <jonatansalas@live.com.ar>
2016-08-10 09:30:56 -0300

    #2 Start to develop FileUpload

--- src/components/field/FileUpload.js
+++ src/components/field/FileUpload.js
@@ -1,5 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import fetch from 'isomorphic-fetch';
 
 export default class FileUpload extends Component {
+	static propTypes = {
+		url: PropTypes.string.isRequired
+	};
+
+	onDrop = (files) => {
+		const { url } = this.props;
+
+		fetch()
+			.then(response => response.json())
+			.then()
+			.catch()
+	};
+
+	render() {
+		return false;
+	}
 
 }
