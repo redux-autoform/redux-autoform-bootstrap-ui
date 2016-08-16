e94d79af25121172c98ad837db259a9e7b4b0f76
daniel.gomez <daniel.gomez@samsistemas.com.ar>
2016-08-16 15:58:31 -0300

    #36 Start using Root componente in Bootstrap (Experimental)

--- src/components/common/Root.js
+++ src/components/common/Root.js
@@ -1,8 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 
-const Root = ({fieldLayout, children, handleSubmit}) => {
-
+const Root = ({fieldLayout, children, handleSubmit, buttonBar, submitting}) => {
 
     // Setting the form class name to "form-horizontal" only affects Bootstrap.
     // ToDo: Make the UI to provide the form class name or the entire Form component
@@ -12,6 +11,7 @@ const Root = ({fieldLayout, children, handleSubmit}) => {
         <div className="meta-form">
             <form onSubmit={handleSubmit} className={formClassName}>
                 { children }
+                { React.createElement(buttonBar, { submitting: submitting }) }
             </form>
         </div>
     );
