3771c870fd5b3dbecb6d9a0c37be9a32f7cbcc03
André Pena <andrerpena@gmail.com>
2016-09-07 10:21:10 -0300

    Just minor change. I was going to make more changes but I'll let that up to Jonatan as he seems to be doing this already

--- demo/Routes.js
+++ demo/Routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import App from './containers/AppContainer';
+import AppContainer from './containers/AppContainer';
 import Demo from './pages/Demo.js';
 
 import { Route, Redirect } from 'react-router';
 
 export default (
-    <Route component={App}>
+    <Route component={AppContainer}>
         <Route path="/redux-autoform-bootstrap-ui/demo.html" component={Demo}/>
         <Redirect from="*" to="/redux-autoform-bootstrap-ui/demo.html" />
     </Route>
