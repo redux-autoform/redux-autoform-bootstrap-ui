9a1142232f7b7d03b4d5f68462c5ca3780c64090
jonatansalas <jonatansalas@live.com.ar>
2016-10-19 23:48:07 -0300

    #48 Switch EntityContainer code to es6 syntax

--- src/components/field/EntityContainer.js
+++ src/components/field/EntityContainer.js
@@ -1,37 +1,93 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const EntityContainer = React.createClass({
+class EntityContainer extends Component {
 
-    propTypes: {
-        name: React.PropTypes.string.isRequired,
-        componentFactory: React.PropTypes.object.isRequired
-    },
+    _renderHeader = () => {
+        let { displayName } = this.props;
 
-    render: function () {
+        if (displayName) {
+            return (
+                <header className="metaform-group-header no-lateral-margin">
+                    <span className="metaform-group-title">
+                        {displayName}
+                    </span>
+                </header>
+            );
+        }
 
-        var header = this.props.displayName
-            ? <header className="metaform-group-header no-lateral-margin">
-                <span className="metaform-group-title">
-                    {this.props.displayName}
-                </span>
-        </header>
-            : null;
+        return null;
+    };
 
+    _renderComponents = () => {
+        let { componentFactory, layout, fields } = this.props;
 
-        return <div className="entity-container">
-            {header}
-            <div className="entity-container-content">
-                {
-                    this.props.componentFactory.buildGroupComponent({
-                        component: this.props.layout.component,
-                        layout: this.props.layout,
-                        fields: this.props.fields,
-                        componentFactory: this.props.componentFactory
-                    })
-                }
+        return componentFactory.buildGroupComponent({
+            component: layout.component,
+            layout: layout,
+            fields: fields,
+            componentFactory: componentFactory
+        })
+    };
+
+    render() {
+        return (
+            <div className="entity-container">
+                {this._renderHeader()}
+                <div className="entity-container-content">
+                    {this._renderComponents()}
+                </div>
             </div>
-        </div>;
+        );
     }
-});
+}
+
+EntityContainer.props = {
+    //Number props
+    innerSize: PropTypes.number,
+
+    //Any props
+    value: PropTypes.any,
+    options: PropTypes.any,
+
+    //Bool props
+    checked: PropTypes.bool,
+    valid: PropTypes.bool,
+    invalid: PropTypes.bool,
+    dirty: PropTypes.bool,
+    pristine: PropTypes.bool,
+    active: PropTypes.bool,
+    touched: PropTypes.bool,
+    visited: PropTypes.bool,
+    autofilled: PropTypes.bool,
+    required: PropTypes.bool,
+
+    //String props
+    group: PropTypes.string,
+    addonBefore: PropTypes.string,
+    addonAfter: PropTypes.string,
+    component: PropTypes.string,
+    help: PropTypes.string,
+    placeholder: PropTypes.string,
+    name: PropTypes.string,
+    error: PropTypes.string,
+    type: PropTypes.string,
+    displayName: PropTypes.string,
+    initialValue: PropTypes.string,
+    fieldLayout: PropTypes.string,
+
+    //Function props
+    autofill: PropTypes.func,
+    onBlur: PropTypes.func,
+    onDragStart: PropTypes.func,
+    onDrop: PropTypes.func,
+    onFocus: PropTypes.func,
+    onUpdate: PropTypes.func,
+    onChange: PropTypes.func,
+
+    //Object props
+    componentFactory: PropTypes.object,
+    reduxFormProps: PropTypes.object,
+    _extra: PropTypes.object
+};
 
 export default EntityContainer;
